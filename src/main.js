f6ce47a76889a380a98225de267a6827d0d69efb
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-08-23 13:35:05 -0400

    spiral map pixel data for wrapping around the sphere

--- src/main.js
+++ src/main.js
@@ -43,8 +43,12 @@ function init()
     canvas.height = img.height;
     canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
 
-    var pixelData = canvas.getContext('2d').getImageData(75, 75, 25, 25).data;
-    console.log(pixelData);
+    var pixelData = canvas.getContext('2d').getImageData(0, 0, img.width, img.height).data;
+    //console.log(pixelData);
+
+    var pixelMap = spiralMap(7);
+    console.log(pixelMap);
+    var sphereizedData = sphereMap(pixelMap, pixelData);
 
 	scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -56,14 +60,15 @@ function init()
     // this material causes a mesh to use colors assigned to faces
 	var faceColorMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
     
-    sphereGeometry = new THREE.SphereGeometry( 80, 32, 16 );
-    console.log('size', sphereGeometry.faces.length);
-    for ( var i = 0; i < sphereGeometry.faces.length; i++ ) 
+    sphereGeometry = new THREE.SphereGeometry( 80, 8, 8 );
+    //console.log('size', sphereGeometry.faces.length);
+    for ( var i = 0; i < sphereizedData.length; i++ ) 
     {
         let face = sphereGeometry.faces[ i ];
+        face.color.setRGB( sphereizedData[i][0], sphereizedData[i][1], sphereizedData[i][2] );
         //console.log('setting color',(pixelData[((i*4)+0)]/255),(pixelData[((i*4)+1)]/255),(pixelData[((i*4)+2)]/255))
         //face.color.setRGB( pixelData[((i*4)+0)], pixelData[((i*4)+1)], pixelData[((i*4)+2)] );
-        face.color.setRGB( (pixelData[((i*4)+0)]/255),(pixelData[((i*4)+1)]/255),(pixelData[((i*4)+2)]/255) );
+        //face.color.setRGB( (pixelData[(((pixelMap[i]-1)*4)+0)]/255),(pixelData[(((pixelMap[i]-1)*4)+1)]/255),(pixelData[(((pixelMap[i]-1)*4)+2)]/255) );
         // if( i > sphereGeometry.faces.length * 0.666){
         //     face.color.setRGB( 0, 0, 0.8 * Math.random() + 0.2 );
         // }else if( i > sphereGeometry.faces.length * 0.5){
@@ -80,6 +85,84 @@ function init()
     camera.position.z = 200;
 
 	
+}
+
+function sphereMap(pixelMap, pixelData){
+
+    let result = [];
+    for ( var i = 0; i < pixelMap.length; i++ ) 
+    {
+        result.push([(pixelData[(((pixelMap[i]-1)*4)+0)]/255),(pixelData[(((pixelMap[i]-1)*4)+1)]/255),(pixelData[(((pixelMap[i]-1)*4)+2)]/255)])
+
+    }
+    return result;
+}
+
+function spiralMap(dimension){
+
+    let result = [];
+
+    const idxRef = generateMatrix(dimension);
+    let top = 0;
+    let bottom = idxRef.length - 1;
+    let left = 0;
+    let right = idxRef[0].length - 1;
+    let dir = 1;
+
+    while (top <= bottom && left <= right) {
+        if (dir == 1) {    // left-right
+            for (let i = left; i <= right; ++i) {
+                //console.log(idxRef[top][i] + " ");
+                result.unshift(idxRef[top][i]);
+            }
+ 
+            ++top;
+            dir = 2;
+        } else if (dir == 2) {     // top-bottom
+            for (let i = top; i <= bottom; ++i) {
+                //console.log(idxRef[i][right] + " ");
+                result.unshift(idxRef[i][right]);
+            }
+ 
+            --right;
+            dir = 3;
+        } else if (dir == 3) {     // right-left
+            for (let i = right; i >= left; --i) {
+                //console.log(idxRef[bottom][i] + " ");
+                result.unshift(idxRef[bottom][i]);
+            }
+ 
+            --bottom;
+            dir = 4;
+        } else if (dir == 4) {     // bottom-up
+            for (let i = bottom; i >= top; --i) {
+                //console.log(idxRef[i][left] + " ");
+                result.unshift(idxRef[i][left]);
+            }
+ 
+            ++left;
+            dir = 1;
+        }
+    }
+
+    //TODO: This only works for a 7x7 grid. Need a way to handle removing the center of a bigger grid
+    if(result%2 !== 0) result = result.splice(1);
+
+    return result;
+}
+
+function generateMatrix(dimension){
+    let result = [];
+
+    // Create the matrix
+    for(let idx=0; idx<dimension; idx++){
+        let row = [];
+        for(let jdx=0; jdx<dimension; jdx++){
+            row.push((jdx+1)+(idx*dimension));
+        }
+        result.push(row);
+    }
+    return result;
 }
 
 function toString(v) { return "[ " + v.x + ", " + v.y + ", " + v.z + " ]"; }
