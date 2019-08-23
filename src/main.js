2671fdc72ec4d15ea4f3733dccce60033e6d8c44
Matthew Wilber <mwilber@Matthews-Mac-mini.local>
2019-08-22 21:30:26 -0400

    set up hello world cube

--- src/main.js
+++ src/main.js
@@ -11,8 +11,60 @@
  * check out the GreenZeta 10 minute PWA example at https://github.com/mwilber/gz-10-minute-pwa
  */ 
 import '../app-shell.css';
-import './serviceWorkerRegistration';
+//import './serviceWorkerRegistration';
 
 // Load application styles
 import '../styles/main.scss';
 
+import * as THREE from 'three';
+
+
+/*
+	Three.js "tutorials by example"
+	Author: Lee Stemkoski
+	Date: July 2013 (three.js v59dev)
+*/
+// MAIN
+// standard global variables
+var container, scene, camera, renderer, controls, stats;
+var clock = new THREE.Clock();
+// custom global variables
+var targetList = [];
+var projector, mouse = { x: 0, y: 0 };
+init();
+animate();
+// FUNCTIONS 		
+function init() 
+{
+	scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+	
+}
+
+function toString(v) { return "[ " + v.x + ", " + v.y + ", " + v.z + " ]"; }
+function animate() 
+{
+    requestAnimationFrame( animate );
+	render();		
+	update();
+}
+function update()
+{
+
+}
+function render() 
+{
+	renderer.render( scene, camera );
+}
